ca7ba6cd74144dec888b76b922449c19a4010127
Hope SKHOSANA <twinsnpods@gmail.com>
2025-08-30 15:37:08 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Locationinfo from './components/Locationinfo'
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Locationinfo from './components/LocationInfo';
 import Footer from './components/Footer';
 import MyMusic from './components/MyMusic';
 import Videos from './components/Videos';
